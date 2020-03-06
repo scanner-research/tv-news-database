4226c4aeed799ebe2ab0defb0c064e5f3ac1dd6a
benhannel <ben@bensprojects.com>
2020-03-05 18:33:10 -0800

    Play nice with quoted commas, autogenerated schema, and metadata reflection

--- import.py
+++ import.py
@@ -1,15 +1,15 @@
 import psycopg2
 import os
 from sqlalchemy import create_engine, MetaData
-from schema import *
+import schema
 import inspect
 import sys
+import time
 
-tables = inspect.getmembers(sys.modules["schema"],
-	lambda member: inspect.isclass(member) and member.__module__ == "schema")
+tables = list(map(str, schema.Face.metadata.sorted_tables))
 
-# These are really big and painful to reimport
-exclude = ['Face', 'Frame']
+# These are really big to reimport
+exclude = ['face', 'frame', 'commercial']
 
 password = os.getenv("POSTGRES_PASSWORD")
 engine = create_engine("postgresql://admin:{}@localhost/postgres".format(password))
@@ -19,28 +19,28 @@ cur = conn.cursor()
 
 # Drop the old tables so we can recreate the schema.
 # MetaData().drop_all(engine)
-for class_name, table in tables:
-	# These are really big and painful to reimport
-	if class_name in exclude:
+for table in tables:
+	if table in exclude:
 		continue
 	print("Dropping", table)
-	cur.execute("DROP TABLE IF EXISTS public.{} CASCADE;".format(table.__tablename__));
+	cur.execute("DROP TABLE IF EXISTS public.{} CASCADE;".format(table));
 conn.commit()
 
 # Create the tables. The metadata object of a single table is aware of all
 # tables, and knows the order in which to create them.
-tables[0][1].metadata.create_all(engine)
+schema.Face.metadata.create_all(engine)
 
 # Populate the tables with data
-for class_name, table in tables:
-	if class_name in exclude:
+for table_name in tables:
+	if table_name in exclude:
 		continue
-	table_name = table.__tablename__
+	pretime = time.time()
 	print("Loading", table_name)
 	fd = open("/newdisk/pg/query_{}.csv".format(table_name))
 	fd.readline() # Skip the headers
-	cur.copy_from(fd, table_name, sep=",", null="")
+	cur.copy_expert("copy {} from stdin (format csv)".format(table_name), fd)
 	conn.commit()
+	print("Loaded {} in {:.3f} seconds".format(table_name, time.time() - pretime))
 
 cur.close()
 conn.close()
