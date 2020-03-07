c2d60b09cc5b3768a2913341d1d78a254134cb85
benhannel <ben@bensprojects.com>
2020-03-07 13:57:08 -0800

    Get rid of old import script

--- import.py
+++ import.py
@@ -1,52 +1,158 @@
-import psycopg2
 import os
-from sqlalchemy import create_engine, MetaData
+import csv
+from datetime import datetime
+from pytz import timezone
 import schema
-import inspect
-import sys
-import time
+import sqlalchemy
+import psycopg2
+import tqdm
 
-tables = list(map(str, schema.Face.metadata.sorted_tables))
 
-# These are really big to reimport
-exclude = [] # 'face', 'frame', 'commercial', 'segment']
+def parse_video_name(name: str):
+    if name.endswith('.mp4'):
+        name = os.path.splitext(name)[0]
+    channel, ymd, hms, show = name.split('_', 3)
+    show = show.replace("_", " ")
+    if channel in ['CNNW', 'FOXNEWSW', 'MSNBCW']:
+        channel = channel[:-1]
+    timestamp = datetime.strptime(ymd + hms, '%Y%m%d%H%M%S')
+    return channel, show, timestamp
 
-password = os.getenv("POSTGRES_PASSWORD")
-engine = create_engine("postgresql://admin:{}@localhost/postgres".format(password))
+def get_or_create(model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        return instance
 
-conn = psycopg2.connect(dbname="tvnews", user="admin", host='localhost', password=password)
-cur = conn.cursor()
+def load_videos(video_csv, show_to_canonical_show_csv):
+    canonical_show_dict = {}
+    with open(show_to_canonical_show_csv) as fp:
+        fp.readline() # Skip headers
+        reader = csv.reader(fp)
+        for row in reader:
+            show, canonical_show, is_recurring = row
+            is_recurring = is_recurring.upper() == 'TRUE'
+            canonical_show_dict[show] = (canonical_show, is_recurring)
 
-# Drop the old tables so we can recreate the schema.
-# Reverse the order to avoid violating dependencies.
-# MetaData().drop_all(engine)
-# for table in reversed(tables):
-# 	if table in exclude:
-# 		continue
-# 	print("Dropping", table)
-# 	cur.execute("DROP TABLE IF EXISTS public.{};".format(table));
-# conn.commit()
+    with open(video_csv) as fp:
+        fp.readline() # Skip headers
+        reader = csv.reader(fp)
+        for row in tqdm.tqdm(reader):
+            (
+                vid, name, num_frames, fps, width, height,
+                is_duplicate, is_corrupt
+            ) =  row
+            vid = int(vid)
+            num_frames = int(num_frames)
+            fps = float(fps)
+            width = int(width)
+            height = int(height)
+            is_duplicate = is_duplicate[0].upper() == 'T'
+            is_corrupt = is_corrupt[0].upper() == 'T'
+            channel, show, timestamp = parse_video_name(name)
+            canonical_show, is_recurring = canonical_show_dict.get(show, (None, False))
 
-# Create the tables. The metadata object of a single table is aware of all
-# tables, and knows the order in which to create them.
-schema.Face.metadata.create_all(engine)
+            # create Channel if needed
+            channel_object = get_or_create(schema.Channel, name=channel)
 
-# Populate the tables with data. They are sorted in dependency order.
-for table_name in tables:
-	if table_name in exclude:
-		continue
-	pretime = time.time()
-	print("Loading", table_name)
-	fd = open("/newdisk/trimmed/{}.csv".format(table_name))
-	headers = fd.readline() # Skip the headers
-	print(headers)
-	try:
-		cur.copy_expert("copy {}({}) from stdin (format csv)".format(table_name, headers), fd)
-		conn.commit()
-		print("Loaded {} in {:.3f} seconds".format(table_name, time.time() - pretime))
-	except psycopg2.errors.UniqueViolation as e:
-		print("Duplicate key", e)
-		conn.rollback()
+            # create CanonicalShow if needed
+            if canonical_show is None:
+                print('Missing canonical show for show:', show)
+                canonical_show = show
+            canonical_show_object = get_or_create(schema.CanonicalShow,
+                name=canonical_show, is_recurring=is_recurring,
+                channel_id=channel_object.id)
 
-cur.close()
-conn.close()
+            # create Show if needed
+            show_object = get_or_create(schema.Show,
+                name=show,
+                channel_id=channel_object.id,
+                canonical_show_id=canonical_show_object.id)
+
+            # create the Video
+            session.add(schema.Video(name=name, num_frames=num_frames, fps=fps,
+                width=width, height=height, time=timestamp,
+                show_id=show_object.id, is_duplicate=is_duplicate, is_corrupt=is_corrupt))
+
+    session.commit()
+
+def load_hosts_staff(host_staff_csv):
+    conn = engine.connect()
+    with open(host_staff_csv) as fp:
+        fp.readline() # Skip headers
+        reader = csv.reader(fp)
+        for row in tqdm.tqdm(reader):
+            channel_name, canonical_show_name, identity_name = row
+
+            s = sqlalchemy.select([schema.Identity]).where(
+                schema.Identity.name == identity_name)
+            identity_id = conn.execute(s).fetchone()['id']
+            
+            if channel_name != '':
+                s = sqlalchemy.select([schema.Channel]).where(
+                    schema.Channel.name == channel_name)
+                channel_id = conn.execute(s).fetchone()['id']
+
+                # create new host_staff_row
+                session.add(schema.HostsAndStaff(identity_id=identity_id,
+                    channel_id=channel_id))
+            
+            if canonical_show_name != '':
+                s = sqlalchemy.select([schema.CanonicalShow]).where(
+                    schema.CanonialShow.name == canonical_show_name)
+                canonical_show_id = conn.execute(s).fetchone()['id']
+                
+                # create new host_staff_row
+                session.add(schema.HostsAndStaff(identity_id=identity_id,
+                    canonical_show_id=canonical_show_id))
+
+    session.commit()
+
+def load_via_copy(import_path, table):
+    source_csv = "{}{}.csv".format(import_path, table)
+    with open(source_csv) as fp:
+        headers = fp.readline() # Skip the headers
+        print("Importing {} with columns ({})".format(table, headers[:-1]))
+
+        cur.copy_expert("copy {}({}) from stdin (format csv)".format(table, headers), fp)
+        conn.commit()
+
+if __name__ == '__main__':
+    password = os.getenv("POSTGRES_PASSWORD")
+    engine = sqlalchemy.create_engine("postgresql://admin:{}@localhost/tvnews".format(password))
+
+    conn = psycopg2.connect(dbname="tvnews", user="admin", host='localhost', password=password)
+    cur = conn.cursor()
+
+    schema.Face.metadata.drop_all(engine)
+    schema.Face.metadata.create_all(engine)
+
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    import_path = '/newdisk/export_db/'
+
+    # Load the rest of the tables, that don't require ETL, with COPY.
+    # This order matters. It must obey dependencies.
+    load_via_copy(import_path, "labeler")
+    load_via_copy(import_path, "frame_sampler")
+    load_via_copy(import_path, "gender")
+    load_via_copy(import_path, "identity")
+
+    # videos, channel, show, and canonical_show require special pre-processing
+    load_videos(import_path + 'video.csv', import_path + 'show_to_canonical_show.csv')
+
+    # hosts_and_staff requires special processing as well. It depends on the
+    # identity table.
+    load_hosts_staff(import_path + 'hosts_and_staff.csv')
+
+    # These tables depend on video, and must be loaded after it
+    load_via_copy(import_path, "frame")
+    load_via_copy(import_path, "face")
+    load_via_copy(import_path, "face_gender")
+    load_via_copy(import_path, "face_identity")
+    load_via_copy(import_path, "commercial")
+    
